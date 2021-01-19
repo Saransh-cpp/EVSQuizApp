451d6c95cb25cd394baa256fd03126011bf15e95
Saransh <saransh0701@gmail.com>
2021-01-19 15:03:51 +0530

    Created Quiz page with page view using quiz widgets created before

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,4 +1,5 @@
 import 'package:evs_quiz_app/provider/user.dart';
+import 'package:evs_quiz_app/screens/Quiz.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:evs_quiz_app/widgets.dart';
@@ -116,15 +117,7 @@ class _HomeState extends State<Home> {
             ),
             MaterialButton(
               onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (c) => QuizCard(
-                  QuestionNumber: '1',
-                  question: 'Test?',
-                  option1: 'Yes',
-                  option2: 'dk',
-                  option3: 'No',
-                  option4: 'probably',
-                  correctOption: 'Yes',
-                )));
+                Navigator.push(context, MaterialPageRoute(builder: (c) => Quiz()));
               },
               child: Text('Quiz'),
             )
