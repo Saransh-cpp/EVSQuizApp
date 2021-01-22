158e1230399a0dc90fb4a160d817c09d401c5ad2
Saransh <saransh0701@gmail.com>
2021-01-22 13:16:13 +0530

    Made the question widget reusable

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,10 +1,8 @@
 import 'package:evs_quiz_app/provider/user.dart';
-import 'package:evs_quiz_app/screens/Quiz.dart';
 import 'package:evs_quiz_app/screens/quizQuestionScreens/Question.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:evs_quiz_app/widgets.dart';
-import 'package:evs_quiz_app/screens/quizQuestionScreens/Question1.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -119,9 +117,80 @@ class _HomeState extends State<Home> {
             ),
             MaterialButton(
               onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (c) => Question()));
+                Navigator.push(context, MaterialPageRoute(builder: (c) => Question(
+                  question1: 'Test1',
+                  question2: 'test2',
+                  question3: 'test3',
+                  question4: 'test4',
+                  question5: 'test5',
+                  question6: 'test6',
+                  question7: 'test7',
+                  question8: 'test8',
+                  question9: 'test9',
+                  question10: 'test10',
+
+                  question1option1: 'yes',
+                  question1option2: 'no',
+                  question1option3: 'probably',
+                  question1option4: 'dk',
+                  question1correctoption: 'yes',
+
+                  question2option1: 'yes',
+                  question2option2: 'no',
+                  question2option3: 'probably',
+                  question2option4: 'dk',
+                  question2correctoption: 'yes',
+
+                  question3option1: 'yes',
+                  question3option2: 'no',
+                  question3option3: 'probably',
+                  question3option4: 'dk',
+                  question3correctoption: 'yes',
+
+                  question4option1: 'yes',
+                  question4option2: 'no',
+                  question4option3: 'probably',
+                  question4option4: 'dk',
+                  question4correctoption: 'yes',
+
+                  question5option1: 'yes',
+                  question5option2: 'no',
+                  question5option3: 'probably',
+                  question5option4: 'dk',
+                  question5correctoption: 'yes',
+
+                  question6option1: 'yes',
+                  question6option2: 'no',
+                  question6option3: 'probably',
+                  question6option4: 'dk',
+                  question6correctoption: 'yes',
+
+                  question7option1: 'yes',
+                  question7option2: 'no',
+                  question7option3: 'probably',
+                  question7option4: 'dk',
+                  question7correctoption: 'yes',
+
+                  question8option1: 'yes',
+                  question8option2: 'no',
+                  question8option3: 'probably',
+                  question8option4: 'dk',
+                  question8correctoption: 'yes',
+
+                  question9option1: 'yes',
+                  question9option2: 'no',
+                  question9option3: 'probably',
+                  question9option4: 'dk',
+                  question9correctoption: 'yes',
+
+                  question10option1: 'yes',
+                  question10option2: 'no',
+                  question10option3: 'probably',
+                  question10option4: 'dk',
+                  question10correctoption: 'yes',
+                )));
               },
-              child: Text('Quiz'),
+              child: Text('Quiz1'),
             )
           ],
         )
--- lib/screens/quizQuestionScreens/Question.dart
+++ lib/screens/quizQuestionScreens/Question.dart
@@ -6,33 +6,105 @@ import 'package:evs_quiz_app/widgets.dart';
 import 'package:flutter/material.dart';
 
 class Question extends StatefulWidget {
+
+  final String question1;
+  final String question2;
+  final String question3;
+  final String question4;
+  final String question5;
+  final String question6;
+  final String question7;
+  final String question8;
+  final String question9;
+  final String question10;
+
+  final String question1option1;
+  final String question1option2;
+  final String question1option3;
+  final String question1option4;
+  final String question1correctoption;
+
+  final String question2option1;
+  final String question2option2;
+  final String question2option3;
+  final String question2option4;
+  final String question2correctoption;
+
+  final String question3option1;
+  final String question3option2;
+  final String question3option3;
+  final String question3option4;
+  final String question3correctoption;
+
+  final String question4option1;
+  final String question4option2;
+  final String question4option3;
+  final String question4option4;
+  final String question4correctoption;
+
+  final String question5option1;
+  final String question5option2;
+  final String question5option3;
+  final String question5option4;
+  final String question5correctoption;
+
+  final String question6option1;
+  final String question6option2;
+  final String question6option3;
+  final String question6option4;
+  final String question6correctoption;
+
+  final String question7option1;
+  final String question7option2;
+  final String question7option3;
+  final String question7option4;
+  final String question7correctoption;
+
+  final String question8option1;
+  final String question8option2;
+  final String question8option3;
+  final String question8option4;
+  final String question8correctoption;
+
+  final String question9option1;
+  final String question9option2;
+  final String question9option3;
+  final String question9option4;
+  final String question9correctoption;
+
+  final String question10option1;
+  final String question10option2;
+  final String question10option3;
+  final String question10option4;
+  final String question10correctoption;
+
+  const Question({Key key, this.question1, this.question2, this.question3,
+    this.question4, this.question5, this.question6, this.question7, this.question8,
+    this.question9, this.question10, this.question1option1, this.question1option2,
+    this.question1option3, this.question1option4, this.question1correctoption,
+    this.question2option1, this.question2option2, this.question2option3,
+    this.question2option4, this.question2correctoption, this.question3option1,
+    this.question3option2, this.question3option3, this.question3option4,
+    this.question3correctoption, this.question4option1, this.question4option2,
+    this.question4option3, this.question4option4, this.question4correctoption,
+    this.question5option1, this.question5option2, this.question5option3,
+    this.question5option4, this.question5correctoption, this.question6option1,
+    this.question6option2, this.question6option3, this.question6option4,
+    this.question6correctoption, this.question7option1, this.question7option2,
+    this.question7option3, this.question7option4, this.question7correctoption,
+    this.question8option1, this.question8option2, this.question8option3,
+    this.question8option4, this.question8correctoption, this.question9option1,
+    this.question9option2, this.question9option3, this.question9option4,
+    this.question9correctoption, this.question10option1, this.question10option2,
+    this.question10option3, this.question10option4, this.question10correctoption
+  }) : super(key: key);
+
   @override
   _QuestionState createState() => _QuestionState();
 }
 
 
 class _QuestionState extends State<Question> {
-
-  var questions = [
-    'test1',
-    'test2',
-    'test3',
-    'test4'
-  ];
-
-  var options = [
-    ['1', '2', '3', '4'],
-    ['5', '6', '7', '8'],
-    ['9', '10', '11', '12'],
-    ['13', '14', '15', '16']
-  ];
-
-  var correctOptions = [
-    '1',
-    '6',
-    '11',
-    '16'
-  ];
 
   Timer _timer;
   int _start = 10;
@@ -72,7 +144,7 @@ class _QuestionState extends State<Question> {
     _timer = new Timer.periodic(
       oneSec,
           (Timer timer) {
-        if (questionNumber < questions.length - 1) {
+        if (questionNumber < 10 - 1) {
           setState(() {
             questionNumber += 1;
             _start = 10;
@@ -90,6 +162,46 @@ class _QuestionState extends State<Question> {
 
   @override
   Widget build(BuildContext context) {
+
+    var questions = [
+      widget.question1,
+      widget.question2,
+      widget.question3,
+      widget.question4,
+      widget.question5,
+      widget.question6,
+      widget.question7,
+      widget.question8,
+      widget.question9,
+      widget.question10
+    ];
+
+    var options = [
+      [widget.question1option1, widget.question1option2, widget.question1option3, widget.question1option4],
+      [widget.question2option1, widget.question2option2, widget.question2option3, widget.question2option4],
+      [widget.question3option1, widget.question3option2, widget.question3option3, widget.question3option4],
+      [widget.question4option1, widget.question4option2, widget.question4option3, widget.question4option4],
+      [widget.question5option1, widget.question5option2, widget.question5option3, widget.question5option4],
+      [widget.question6option1, widget.question6option2, widget.question6option3, widget.question6option4],
+      [widget.question7option1, widget.question7option2, widget.question7option3, widget.question7option4],
+      [widget.question8option1, widget.question8option2, widget.question8option3, widget.question8option4],
+      [widget.question9option1, widget.question9option2, widget.question9option3, widget.question9option4],
+      [widget.question10option1, widget.question10option2, widget.question10option3, widget.question10option4]
+    ];
+
+    var correctOptions = [
+      widget.question1correctoption,
+      widget.question2correctoption,
+      widget.question3correctoption,
+      widget.question4correctoption,
+      widget.question5correctoption,
+      widget.question6correctoption,
+      widget.question7correctoption,
+      widget.question8correctoption,
+      widget.question9correctoption,
+      widget.question10correctoption
+    ];
+
     return SafeArea(
       child: Scaffold(
         body: Center(
